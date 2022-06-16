a18e4c6275e8047531f7ea45c0bb92c06c2826dd
joeleung32 <joeleung32@yahoo.com.hk>
2022-06-17 01:05:00 +0800

    SQLite database prepared

---
+++ db/init/createTables.sql
@@ -0,0 +1,40 @@
+create table Administrator
+(
+    id       INTEGER not null
+        constraint id
+            primary key autoincrement,
+    username TEXT    not null,
+    password TEXT    not null,
+    createAt TEXT,
+    isValid  INTEGER default 0 not null
+);
+
+create unique index Administrator_username_unique
+    on Administrator (username);
+
+create table AdministratorToken
+(
+    id       INTEGER not null
+        constraint id
+            primary key autoincrement,
+    adminId  INTEGER not null,
+    token    TEXT    not null,
+    createAt TEXT,
+    expiryAt TEXT,
+    isValid  INTEGER default 0 not null
+);
+
+create table ApiToken
+(
+    id          INTEGER
+        constraint id
+            primary key autoincrement,
+    token       TEXT not null,
+    description TEXT,
+    createAt    TEXT,
+    expiryAt    INTEGER,
+    isValid     INTEGER default 0 not null
+);
+
+create unique index ApiToken_token_unique
+    on ApiToken (token);
---
+++ db/init/insertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO Administrator (id, username, password, createAt, isValid) VALUES (1, 'admin', '$2b$10$Ead11MjeIefcMYJcfBI/teJdoxjObreR5a6YUClGYn6n2wagC6AMS', null, 1);
+
+INSERT INTO ApiToken (id, token, description, createAt, expiryAt, isValid) VALUES (1, 'd897a87b-ac1e-47c6-8571-cfda576f3c2d', 'Admin Panel - Dev', null, null, 1);
