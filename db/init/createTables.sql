19d077c9d043f4ecdb0e400fa8d4d2f7018b7d98
joeleung32 <joeleung32@yahoo.com.hk>
2022-06-22 23:42:30 +0800

    Database added tables: ContentObject and ContentData

--- db/init/createTables.sql
+++ db/init/createTables.sql
@@ -38,3 +38,27 @@ create table ApiToken
 
 create unique index ApiToken_token_unique
     on ApiToken (token);
+
+create table ContentObject
+(
+    id             INTEGER not null
+        constraint id
+            primary key autoincrement,
+    object_name    TEXT    not null,
+    object_catalog TEXT
+);
+
+create table ContentData
+(
+    id            INTEGER not null
+        constraint id
+            primary key autoincrement,
+    title         TEXT,
+    content       TEXT,
+    createAt      TEXT,
+    updateAt      TEXT,
+    isPublished   INTEGER default 0,
+    contentObject TEXT
+);
+
+
